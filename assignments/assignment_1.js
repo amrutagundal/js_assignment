af1532d7b04fd57967d450d706872955f3c0e7c0
amrutagundal <106256752+amrutagundal@users.noreply.github.com>
2022-06-13 20:24:34 +0530

    Add files via upload

---
+++ assignments/assignment_1.js
@@ -0,0 +1,15 @@
+/*My first javascript assignment */
+console.log('Tell me about yourself?');
+console.log('My name is Amruta Gundal');
+console.log('I am engineering graduate');
+console.log('I have completed my B.E in electronic and telecommunication engineering in 2016 from pune university');
+console.log('I have 3.5 year experience of working as software engineer.I got Opportunity to work on different projects');
+console.log('=======================================================================================================');
+console.log('Technology stack :HTML5,CSS3,Bootstrap,JavaScript ES6 ,Angular 8');
+console.log('Tools used: GIT,GITHUB');
+console.log('Project development method: Agile Methodology');
+console.log('********************************************************************************************************');
+console.log('Currently not working');
+console.log('###################################################################################################');
+console.log('Today I have Learnt:How to install node.js and done my first helloworld javascipt program');
+console.log('**********************************************************************************');
